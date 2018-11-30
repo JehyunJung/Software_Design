9c5154de76669239f16a7dc0abf333ca441ccb68
pooya98 <pooya98@naver.com>
2018-12-01 02:25:00 +0900

    1201 first

--- Software_design/src/Person/Student.java
+++ Software_design/src/Person/Student.java
@@ -3,16 +3,17 @@ package Person;
 import java.util.LinkedList;
 import java.util.List;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-import Application.$missing$;
+import Application.*;
 import Bulletin.*;
 import Status.*;
 
 public class Student extends Person {
 	String score;
-	Status myStatus;
+	
 
 	public Student(String name, String number, String score) {
 		super(name, number);
@@ -35,8 +36,8 @@ public class Student extends Person {
 	}
 
 	public boolean firstapply() { // see appliable Bulletin and apply
-
-		if (myStatus.first_application_check() == false)
+		
+		if (Status.first_application_check() == false)
 			return false;
 
 		int length, select;
@@ -70,6 +71,9 @@ public class Student extends Person {
 		String major; // exchange student major
 		File file = new File("bulletins.txt");
 		Scanner sc = new Scanner(System.in);
+		Status myStatus = null;
+		
+		
 		try {
 			sc = new Scanner(file);
 		} catch (IOException e) {
@@ -89,7 +93,24 @@ public class Student extends Person {
 			{
 				Bulletin apply_info = new Bulletin(col_name, req_score, country, period, major);
 				List<Course> course = new LinkedList<>();
+				
+				//Create myStatus in 
 				myStatus = new Status(this.name, this.number, -1, -1, -1, apply_info, course);
+				
+				
+				// upload status to DB
+				 try {
+					FileWriter fw = new FileWriter("C:\\Users\\TG\\eclipse-workspace\\Software_design\\src\\Person\\StatusDB.txt",true);
+					 fw.write(this.name);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 
+			//	 fw.write(myStatus);
+				 
+				 
+				 
 			}
 		}
 
